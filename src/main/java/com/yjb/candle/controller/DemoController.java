39cadba931afd53ad11b8ee138ccf154ab16b4cc
Jin <sunj__@naver.com>
2022-08-12 10:50:24 +0900

    spring boot & react

---
+++ src/main/java/com/yjb/candle/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.yjb.candle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
