562b3e99de919b914df721012815b9d4a83fec2d
Jin <sunj__@naver.com>
2022-08-17 13:56:53 +0900

    mapper

--- src/main/java/com/yjb/candle/mapper/TestMapper.java
+++ src/main/java/com/yjb/candle/mapper/TestMapper.java
@@ -1,8 +1,11 @@
 package com.yjb.candle.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.yjb.candle.domain.NameVO;
 import com.yjb.candle.domain.SearchVO;
 
+@Mapper
 public interface TestMapper {
 
 	public NameVO result(SearchVO search);
--- src/main/java/com/yjb/candle/service/TestServiceImpl.java
+++ src/main/java/com/yjb/candle/service/TestServiceImpl.java
@@ -1,15 +1,19 @@
 package com.yjb.candle.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.yjb.candle.domain.NameVO;
 import com.yjb.candle.domain.SearchVO;
 import com.yjb.candle.mapper.TestMapper;
 
+import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Service
+@AllArgsConstructor
 public class TestServiceImpl implements TestService {
 
 	@Setter(onMethod_= @Autowired)
